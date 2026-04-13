6f80f812d7d904acb724f98a4c202fe327fd4dbf
jaspreet <jkaur126@academic.rrc.ca>
2026-04-13 15:25:26 -0500

    Integrated Multer for image upload.

--- movie-api/src/controller/movieController.js
+++ movie-api/src/controller/movieController.js
@@ -1,21 +1,50 @@
 const Movie = require("../models/Movie");
 
+//Updated createMovie with image upload support
 exports.createMovie = async (req, res) => {
-  const movie = await Movie.create(req.body);
-  res.status(201).json(movie);
+  try {
+    const movie = await Movie.create({
+      ...req.body,
+      image: req.file ? req.file.path : null,
+    });
+
+    res.status(201).json(movie);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
+// Get all movies
 exports.getMovies = async (req, res) => {
-  const movies = await Movie.find();
-  res.json(movies);
+  try {
+    const movies = await Movie.find();
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
+// Update movie
 exports.updateMovie = async (req, res) => {
-  const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(movie);
+  try {
+    const movie = await Movie.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.json(movie);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
+// Delete movie
 exports.deleteMovie = async (req, res) => {
-  await Movie.findByIdAndDelete(req.params.id);
-  res.json({ message: "Deleted" });
+  try {
+    await Movie.findByIdAndDelete(req.params.id);
+    res.json({ message: "Deleted" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
