82d51c0e5fbcbec07812a21260b7b3e071ec98ef
jaspreet <jkaur126@academic.rrc.ca>
2026-04-23 18:59:00 -0500

    Modify the files .

--- movie-api/src/app.js
+++ movie-api/src/app.js
@@ -1,16 +1,10 @@
-import express from "express";
-import movieRoutes from "./routes/movieRoutes";
-import swaggerUi from "swagger-ui-express";
-import { swaggerSpec } from ".src/config/swagger";
-import { errorHandler } from "./middleware/errorHandler";
+const express = Require("express");
+const movieRoutes = require("./routes/movieRoutes");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/movies", movieRoutes);
 
-app.use(errorHandler);
-
-export default app;
+module.exports = app;
--- movie-api/src/server.js
+++ movie-api/src/server.js
@@ -1,4 +1,4 @@
-const app = require("./app");
+const app = Require("./app");
 
 const PORT = process.env.PORT || 3000;
 
